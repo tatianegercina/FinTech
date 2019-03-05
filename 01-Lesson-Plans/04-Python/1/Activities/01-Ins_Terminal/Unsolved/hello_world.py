e626952a916724b3da5cdd52ee03b654b005935c
Andrew Yang <andrewyang@AdminisatorsMBP.home>
2019-03-05 12:22:57 -0500

    Adding in python activity

---
+++ 01-Lesson-Plans/04-Python/1/Activities/01-Ins_Terminal/Unsolved/hello_world.py
@@ -0,0 +1,4 @@
+# Use the in-built Python print() function to print a statement to the console to express your first words in Python!
+# Hint: Type up some text in between the double quotes.
+
+print("")
