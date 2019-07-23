aab7823a4fd3da7c512d02015b40d0651b7c4b16
Andrew Yang <ayang@trilogyed.com>
2019-07-23 15:04:21 -0400

    adding solution code

--- 01-Lesson-Plans/03-Python-Pandas/1/Activities/08-loops-logic-01/Solved/loops-logic-01.py
+++ 01-Lesson-Plans/03-Python-Pandas/1/Activities/08-loops-logic-01/Solved/loops-logic-01.py
@@ -1,3 +1,108 @@
+# Declare an empty list named `our_list`.
+our_list = []
+
+# Use the `append` list function to append the number 1 into `our_list`.
+our_list.append(1)
+
+# Append the string "hello" into `our_list`.
+our_list.append("hello")
+
+# Append the boolean False into `our_list`.
+our_list.append(False)
+
+# Append the number 84 into `our_list`.
+our_list.append(84)
+
+# Append the string "world" into `our_list`
+our_list.append("world")
+
+# Print `our_list`
+print(our_list)
+
+# Declare a variable named `one_to_ten` and assign it an list containing the numbers from 1 to 10.
+one_to_ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Print the 4th element from `one_to_ten`.
+print(one_to_ten[3])
+
+# Print the 7th element from `one_to_ten`.
+print(one_to_ten[6])
+
+# We've declared this list for you
+num_list = [2, 65, 3, 7, 39, 22, 11, 94, 299, 9, 20, 21, 51, 37]
+
+# Iterate through the provided `num_list` and create an if-else statement to print every number greater than 50
+for number in num_list:
+    if number > 50:
+        print(number)
+
+# Iterate through the provided `num_list` and use the `index` function to print the index of the first occurrence of the number 11.
+
+
+# Iterate through the provided `num_list` and print the sum of all the numbers.
+
+# Iterate through the provided `num_list` and create an if-else statement to print the sum of all the numbers greater than 50.
+
+# Iterate through the provided `num_list` and create an if-else statement to print the sum of all the even numbers.
+
+# Iterate through the provided `fruits` list and print the number of times "Apple" appears in the list.
+
+# Iterate through the provided `fruits` list and print the number of times "Peach" appears in the list.
+
+# Iterate through the provided `fruits` list and print the number of fruits that start with "P" in the list.
+
+## Challenge
+
+# Create a new empty list named `unique_fruits`.
+
+# Iterate through `fruits` and populate `unique_fruits` with only unique values from `fruits`. Hint: try looking up "not in" conditionals for if statements
+
+# Print out the `unique_fruits` list.
+
+# Iterate through the first list inside `two_dim_list` and print all the numbers less than 25.
+
+# Iterate through the second list inside `two_dim_list` and print all the numbers less than 25.
+
+# Iterate through the fifth list inside `two_dim_list` and print all the numbers less than 25.
+
+# Iterate through `two_dim_list` and the lists inside of it and print all the odd numbers.
+
+# Iterate through `two_dim_list` and the lists inside of it and print the sum of all the numbers that are a multiple of 3.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # declare a variable named our_list and assign an empty list to it
 
 
@@ -23,7 +128,7 @@
 
 
 
-# This seperator is here to make your prints are little easier to read 
+# This seperator is here to make your prints are little easier to read
 # by creating seperations between the different sections of the activity
 print("--------------------------Seperator 1--------------------------")
 
@@ -40,7 +145,7 @@ print("--------------------------Seperator 1--------------------------")
 
 
 
-# This seperator is here to make your prints a little easier to read 
+# This seperator is here to make your prints a little easier to read
 # by creating seperations between the different sections of the activity
 print("--------------------------Seperator 2--------------------------")
 
@@ -49,7 +154,7 @@ num_list = [2, 65, 3, 7, 39, 22, 11, 94, 299, 9, 20, 21, 51, 37]
 # iterate through num_list and create an individual print for every number greater than 50
 
 
-    
+
 # iterate through num_list and print the index of the first occurance for the number 11
 
 
@@ -57,7 +162,7 @@ num_list = [2, 65, 3, 7, 39, 22, 11, 94, 299, 9, 20, 21, 51, 37]
 # iterate through num_list and print the sum of all the numbers
 
 
-  
+
 # iterate through num_list and print the sum of all the numbers greater than 50
 
 
@@ -66,13 +171,13 @@ num_list = [2, 65, 3, 7, 39, 22, 11, 94, 299, 9, 20, 21, 51, 37]
 
 
 
-# This seperator is here to make your prints a little easier to read 
+# This seperator is here to make your prints a little easier to read
 # by creating seperations between the different sections of the activity
 print("--------------------------Seperator 3--------------------------")
 
 # We've declared this list for you
 fruits = [
-  "Apple", "Orange", "Banana", "Pomelo", "Apple", "Kiwi", "Peach", "Banana", "Grape", "Tomato", 
+  "Apple", "Orange", "Banana", "Pomelo", "Apple", "Kiwi", "Peach", "Banana", "Grape", "Tomato",
   "Kiwi", "Apple", "Watermelon", "Lemon", "Pomelo", "Apple", "Banana", "Peach", "Apricot", "Grape"]
 # iterate through fruits and print the number of times "Apple" appears in the list
 
@@ -86,7 +191,7 @@ fruits = [
 
 
 
-# CHALLENGE (Optional!) 
+# CHALLENGE (Optional!)
 # create a new empty list named unique_fruits
 
 
@@ -99,12 +204,12 @@ fruits = [
 
 
 # CHALLENGE (Optional!)
-# This seperator is here to make your prints a little easier to read 
+# This seperator is here to make your prints a little easier to read
 # by creating seperations between the different sections of the activity
 print("--------------------------Seperator 4--------------------------")
 
 
-# In this challenge we're going to be working with nested lists. 
+# In this challenge we're going to be working with nested lists.
 # You can store any type of data within an list- even other lists!
 two_dim_list = [
   [54, 6, 7, 46, 78],
@@ -139,7 +244,7 @@ print("======== odd numbers ========")
 
 # another seperator for your convenience
 print("======== sum of multiples of 3 ========")
-# iterate through two_dim_list and the lists inside of it and 
+# iterate through two_dim_list and the lists inside of it and
 # print the sum of all the numbers that are a multiple of 3
 
 
