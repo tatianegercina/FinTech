9db60a7d39d4972f1d832d9718b46a2d9df2a5c9
Andrew Yang <ayang@trilogyed.com>
2019-07-23 00:01:31 -0400

    adding unsolved counterpart

--- 01-Lesson-Plans/03-Python-Pandas/1/Activities/05-variables-04/Unsolved/variables-04.py
+++ 01-Lesson-Plans/03-Python-Pandas/1/Activities/05-variables-04/Unsolved/variables-04.py
@@ -1,29 +1,28 @@
-# Declare a variable of boba_shop  with an input and a string of "Welcome to the Boba Shop!"
-
-# Check if boba_shop  is equal to true
+# Declare a variable of `name` with an input and a string of "Welcome to the Boba Shop! What is your name?".
 
 
-    # Write a print with a string of "Hello"
+# Check if `name` is not an empty string or equal to `None`.
+
+    # If so, write a print with a string of "Hello" concatenated with the variable `name`.
 
 
-    # Declare a variable of beverage with an input and a string of "What kind of boba drink would you like ?"
+    # Then, declare a variable of `beverage` with an input and a string of "What kind of boba drink would you like?".
 
 
-    # Declare a variable of sweetness with an input and a string of "How sweet do you want your drink 0,50,100,200 ?"
-
-    # Now check 
-        # if sweetness equals to 50 print "half sweetened"
-    
-        # else if sweetness 100 print "normal sweet"
-
-    
-        # else if sweetness 200 print "super sweet"
+    # Then, Declare a variable of `sweetness` with an input and a string of "How sweet do you want your drink: 0, 50, 100, or 200?".
 
 
-        # else print with a string of "non-sweet"
+    # If `sweetness` equals 50 print "half sweetened".
+
+    # Else if `sweetness` 100 print "normal sweet".
+
+    # Else if `sweetness` 200 print "super sweet".
+
+    # Else print with a string of "non-sweet".
 
 
-    # then print with a string of "your order of " variable beverage and a string of " boba with a sweet level of " and variable of sweetness
- 
-    # and print string of "goodbye".
+    # Then print the string of "Your order of " concatenated with the variable `beverage`, concatenated with " boba with a sweet level of ", concatenated with the variable `sweetness`
+
+# Else, print the string of "You didn't give us your name! Goodbye"
+
 
