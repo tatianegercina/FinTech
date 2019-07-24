afe0390e32d0a3e1a1257462ae5f4aebb1a92df5
Andrew Yang <andrew.yang5194@gmail.com>
2019-07-24 05:05:07 -0400

    adding revised README and solved/unsolved counterparts

--- 01-Lesson-Plans/03-Python-Pandas/1/Activities/13-dicts-01/Unsolved/dicts-01.py
+++ 01-Lesson-Plans/03-Python-Pandas/1/Activities/13-dicts-01/Unsolved/dicts-01.py
@@ -23,28 +23,27 @@
 # }
 
 # Re-create the content of the commented out `san_francisco` dictionary by using bracket notation to manually add each of the key-value pairs (including nested objects).
-san_francisco = {}
 
-san_francisco['west_coast'] = True
-san_francisco['has_multiple_bridges'] = True
-san_francisco['known_for_pizza'] = False
-san_francisco['coastal'] = True
-san_francisco['snows'] = False
-san_francisco['very_hot'] = False
-san_francisco['mayor'] = "London Breed"
-san_francisco['state'] = "California"
-san_francisco['country'] = "USA"
-san_francisco['best_food'] = "burritos"
-san_francisco['sports_teams'] = ["Giants", "Warriors", "Forty-Niners"]
-san_francisco['tallest_building'] = "SalesForce Building"
-san_francisco['population'] = 884363
-san_francisco['city_size'] = "large"
-san_francisco['median_house_price'] = 1610000
-san_francisco['famous_residents'] = ["Maya Angelous", "Robert Frost", "Carlos Santana"]
-san_francisco['homeless_pop'] = 1150
-san_francisco['political_leaning'] = "Democrat"
-san_francisco['notable_attractions'] = ["Alcatraz", "Golden Gate Bridge", "Fisherman's Wharf"]
-san_francisco['natural_disasters'] = ["Earthquakes"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Print the manually modified `san_francisco` dictionary and confirm the contents match the commented out version.
-print(san_francisco)
