20e249a5e47b4d77d1493a2c47c2486cceddecb7
Andrew Yang <ayang@trilogyed.com>
2019-07-24 13:08:03 -0400

    saving changes so far

--- 01-Lesson-Plans/03-Python-Pandas/1/Activities/15-algo-challenge-01/Solved/algo-challenge-01.py
+++ 01-Lesson-Plans/03-Python-Pandas/1/Activities/15-algo-challenge-01/Solved/algo-challenge-01.py
@@ -6,23 +6,35 @@ import random
 # QUESTION 1: Create a function called `number_guess` that takes in an integer as an argument.
 # If the number is 42, print(true). If it isn't 42, print(false)
 def number_guess(number):
-    print("replace this print statement with your code")
+    if number == 42:
+        print(True)
+    else:
+        print(False)
 
 # This code is to help you test your function
-test_num = random.randint(40,45)
+test_num = random.randint(40, 45)
 print("Number " + test_num)
 number_guess(test_num)
 
 # QUESTION 2: Write a function that takes in a list of numbers. the function should print the smallest number in the given list
-def find_smallest(list):
-    print("replace this print statement with your code")
+def find_smallest(list_param):
+    minimum = 0
+
+    for number in list_param:
+        if minimum == 0:
+            minimum = number
+        elif number < minimum:
+            minimum = number
+
+    print(f"The smallest number in the list is {minimum}")
 
 # This code is to help you test your function
 nums = [10, 11, 3, 123, 54, 6, 67]
 find_smallest(nums)
-# QUESTION 3: Write a function which takes in a of strings. The function should print the shortest string in the list.
+
+# QUESTION 3: Write a function which takes in a list of strings. The function should print the shortest string in the list.
 def find_shortest(arr):
-    print("replace this print statement with your code")
+
 
 # This code is to help you test your function
 strings = ["hey there", "yo", "a", "hello", "what up", "hello, my name is farley", "howdy"]
