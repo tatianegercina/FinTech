ba5035ab98be63e1b8ae1330df9946f3bf8d5892
Andrew Yang <ayang@trilogyed.com>
2019-07-24 13:22:20 -0400

    adding solved/unsolved activity files

--- 01-Lesson-Plans/03-Python-Pandas/1/Activities/15-algo-challenge-01/Solved/algo-challenge-01.py
+++ 01-Lesson-Plans/03-Python-Pandas/1/Activities/15-algo-challenge-01/Solved/algo-challenge-01.py
@@ -13,7 +13,7 @@ def number_guess(number):
 
 # This code is to help you test your function
 test_num = random.randint(40, 45)
-print("Number " + test_num)
+print(f"Number {test_num}")
 number_guess(test_num)
 
 # QUESTION 2: Write a function that takes in a list of numbers. the function should print the smallest number in the given list
@@ -33,7 +33,26 @@ nums = [10, 11, 3, 123, 54, 6, 67]
 find_smallest(nums)
 
 # QUESTION 3: Write a function which takes in a list of strings. The function should print the shortest string in the list.
-def find_shortest(arr):
+def find_shortest(list_param):
+    shortest_string = ""
+    shortest_character_count = 0
+
+    for string in list_param:
+        count = 0
+
+        for character in string:
+            count += 1
+
+        print(string, count)
+
+        if shortest_character_count == 0:
+            shortest_character_count = count
+            shortest_string = string
+        elif count < shortest_character_count:
+            shortest_character_count = count
+            shortest_string = string
+
+    print(f"The shortest string is {shortest_string}")
 
 
 # This code is to help you test your function
@@ -41,13 +60,17 @@ strings = ["hey there", "yo", "a", "hello", "what up", "hello, my name is farley
 find_shortest(strings)
 
 #QUESTION 4: Write a function that takes in three arguments: a high value, a low value and a list of numbers.
-# The function should print a new list of numbers where none of the elements are less than the low value and none of the elements are greater than the high value
-def filter_list(high, low, arr):
-    print("replace this print statement with your code")
+# The function should print a new list of numbers where the elements are greater than the low value and less than the high value
+def filter_list(high, low, list_param):
+    filtered_list = []
+
+    for number in list_param:
+        if number > 10 and number < 20:
+            filtered_list.append(number)
 
 # This code is to help you test your function
-down = 10
-up = 20
+high = 20
+low = 10
 arr = [2,5,99,15,23,18,11,21]
 
-filter_list(up, down, arr)
+filter_list(high, low, arr)
