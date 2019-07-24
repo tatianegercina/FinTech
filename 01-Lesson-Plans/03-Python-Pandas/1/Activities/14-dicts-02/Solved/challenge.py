182979503296644b20053b7a1198011766e8e9ec
Andrew Yang <andrew.yang5194@gmail.com>
2019-07-24 05:28:16 -0400

    adding revised README

---
+++ 01-Lesson-Plans/03-Python-Pandas/1/Activities/14-dicts-02/Solved/challenge.py
@@ -0,0 +1,77 @@
+# Use the `from` keyword to import the `shows` dictionary from the `show_data.py` file
+from show_data import shows
+
+# QUESTION 1: Who is the actor that plays Squidward in Spongebob (kids)?
+# ANSWER:
+
+# QUESTION: Patrick Warburton plays Joe Swanson in Family Guy (comedy). What is the link to his imdb page?
+# ANSWER:
+
+# QUESTION: Is the Walking Dead still running?
+# ANSWER:
+
+# QUESTION: Who plays Dexter in Dexter (drama) and who plays Dexter in Dexter's Lab (kids)?
+# HINT: You can print multiple items at once by using a comma like this: print(thing1, thing2)
+# ANSWER:
+
+# QUESTION: Who are the creators of Stranger Things (drama)?
+# ANSWER:
+
+# QUESTION: Who hosts the Daily Show (talk)?
+# ANSWER:
+
+# QUESTION: Who are all the hosts of the view (talk)
+# Hint: You will need to use a loop for this one. You may not simply log the entire list, but must log each name individually
+# ANSWER:
+# for ____ in _____
+    #print()
+
+# QUESTION: What are the show names of the Impractical Jokers (comedy)
+# Hint: You will need to use a loop for this one. You may not simply log the entire list, but must log each name individually
+# ANSWER:
+# for ____ in _____
+    #print()
+
+# QUESTION: Who does Will Arnett play in Arrested Development (comedy)
+# ANSWER:
+
+# QUESTION: Who plays Yami Yugi in Yu-Gi-Oh (kids)?
+# ANSWER:
+
+# QUESTION: How many seasons did the Office (comedy) run?
+# ANSWER:
+
+# QUESTION: Who are the main characters of the Office (comedy) (not the actors, but the actual character names)?
+# ANSWER:
+# for ____ in _____
+    #print()
+
+# QUESTION: List the characters in Teen Titans (kids)
+# ANSWER:
+# for ____ in _____
+    #print()
+
+# QUESTION: What is the link to the IMDB page for the actor who plays Mr. Krabs (Spongebob, kids)?
+# ANSWER:
+
+# QUESTION: Who plays Negan in The Walking Dead?
+# ANSWER:
+
+# QUESTION: List the main cast of Dexter (drama) (the actors, not the characters)
+# ANSWER:
+# for ____ in _____
+    #print()
+
+# QUESTION: Is Game of Thrones(drama) still running?
+# ANSWER:
+
+# QUESTION: Who does Peter Dinklage play in Game of Thrones (drama)?
+# ANSWER:
+
+# QUESTION: List the American Idol Judges
+# ANSWER:
+# for ____ in _____
+    #print()
+
+# QUESTION: Who plays Dustin in Stanger Things (drama)?
+# ANSWER:
