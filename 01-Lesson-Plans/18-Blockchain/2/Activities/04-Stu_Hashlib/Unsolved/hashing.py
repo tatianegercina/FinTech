6ab58d1f7387d933c7e65b04ef1aa3e33e3b06a4
Alec M. Wantoch <alec@wantoch.net>
2019-08-28 17:12:46 -0400

    Adds 18.2 SD: Hashing with Hashlib

---
+++ 01-Lesson-Plans/18-Blockchain/2/Activities/04-Stu_Hashlib/Unsolved/hashing.py
@@ -0,0 +1,31 @@
+import hashlib
+
+# output sha256 hash in hexadecimal string format
+def hash(message):
+    return hashlib.sha256(message).hexdigest()
+
+# modify these messages
+# note: we include the "b" before the string definition in order to represent it as bytes instead of a string
+message_one = b""
+
+message_two = b""
+
+# print both messages and their corresponding hashes
+print()
+print()
+
+# compare the hashes in an if/else statement
+
+hash_one =
+
+hash_two =
+
+if ():
+    print()
+else:
+    print()
+
+# compare the length of the hashes
+
+print(len())
+print(len())
