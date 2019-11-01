914a926e2eb8a9a7358bed94b087542a99e7539c
jhillgithub <jhillgithub@users.noreply.github.com>
2019-11-01 13:59:43 -0500

    Updates 15.2.9 code

--- 01-Lesson-Plans/15-Algorithmic-Trading/2/Activities/09-Evr_Streaming_Dashboard/Unsolved/jarvis.py
+++ 01-Lesson-Plans/15-Algorithmic-Trading/2/Activities/09-Evr_Streaming_Dashboard/Unsolved/jarvis.py
@@ -15,22 +15,8 @@ def initialize(cash=None):
     """Initialize the dashboard, data storage, and account balances."""
     print("Intializing account, database, and DataFrame")
 
-    # Initialize Database
-    db = sqlite3.connect("algo_trader_history.sqlite")
-    with db:
-        cur = db.cursor()
-        cur.execute("DROP TABLE IF EXISTS data")
-
-    # Initialize Account
-    account = {"balance": cash, "shares": 0}
-
     # @TODO: Add Streamz DataFrames!
 
-    # Initialize the dashboard
-    dashboard = build_dashboard()
-
-    # @TODO: We will complete the rest of this later!
-    return db, account, df, dashboard
 
 
 def build_dashboard():
@@ -59,21 +45,6 @@ def generate_signals(df):
 
     signals = df.copy()
     # @TODO fix the index for the DataFrame!
-    signals["signal"] = 0.0
-
-    # Generate the short and long moving averages
-    signals["sma10"] = signals["close"].rolling(window=10).mean()
-    signals["sma20"] = signals["close"].rolling(window=20).mean()
-
-    # Generate the trading signal 0 or 1,
-    signals["signal"][short_window:] = np.where(
-        signals["sma10"][short_window:] > signals["sma20"][short_window:], 1.0, 0.0
-    )
-
-    # Calculate the points in time at which a position should be taken, 1 or -1
-    signals["entry/exit"] = signals["signal"].diff()
-
-    return signals
 
 
 def execute_trade_strategy(signals, account):
