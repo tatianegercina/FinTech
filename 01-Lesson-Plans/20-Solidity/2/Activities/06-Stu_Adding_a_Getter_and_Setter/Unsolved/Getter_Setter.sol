94b5cc26e752368711b824d02f7f32eb2c28b86b
jiyuu-jin <jiyuu.llc@gmail.com>
2019-11-18 17:01:43 -0800

    Adds 20.2.6 ID: Adding a Getter and Setter

---
+++ 01-Lesson-Plans/20-Solidity/2/Activities/06-Stu_Adding_a_Getter_and_Setter/Unsolved/Getter_Setter.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.11;
+
+contract SimpleCustomerAccount {
+    address owner;
+    bool newAccount;
+    uint accountBalance;
+    string accountID;
+
+    function getInfo() public returns(){
+        return ();
+    }
+
+    function setInfo(address newOwner, bool isNewAccount, uint newAccountBalance, string memory newAccountID) public{
+    }
+}
