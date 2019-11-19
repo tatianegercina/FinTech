95962ae87f0daf2d6ee5893f67d741f657f1a8eb
jiyuu-jin <jiyuu.llc@gmail.com>
2019-11-18 23:39:52 -0800

    Made requested changes

--- 01-Lesson-Plans/20-Solidity/2/Activities/03-Stu_Building_a_Basic_Contract/Solved/SimpleCustomerAccount.sol
+++ 01-Lesson-Plans/20-Solidity/2/Activities/03-Stu_Building_a_Basic_Contract/Solved/SimpleCustomerAccount.sol
@@ -4,5 +4,5 @@ contract SimpleCustomerAccount {
     address owner = 0xc3879B456DAA348a16B6524CBC558d2CC984722c;
     bool newAccount = true;
     uint accountBalance = 33333333;
-    string customer_name = "Jane Doe";
+    string customerName = "Jane Doe";
 }
