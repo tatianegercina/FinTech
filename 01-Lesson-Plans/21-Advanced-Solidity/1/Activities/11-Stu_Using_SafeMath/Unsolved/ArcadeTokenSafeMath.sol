ed56769f2b9fa9d0136957a6fa91abab67ad1d6e
Alec M. Wantoch <alec@wantoch.net>
2019-12-06 13:33:15 -0500

    21.1.11 Added @TODOs

--- 01-Lesson-Plans/21-Advanced-Solidity/1/Activities/11-Stu_Using_SafeMath/Unsolved/ArcadeTokenSafeMath.sol
+++ 01-Lesson-Plans/21-Advanced-Solidity/1/Activities/11-Stu_Using_SafeMath/Unsolved/ArcadeTokenSafeMath.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.0;
 
-// import SafeMath library via Github URL here
+// @TODO: import the SafeMath library via Github URL
+// YOUR CODE HERE!
 
 contract ArcadeToken {
     // @TODO: add the "using SafeMath..." line here to link the library to all uint types
@@ -17,23 +18,23 @@ contract ArcadeToken {
     }
 
     function transfer(address recipient, uint value) public {
-        // replace the following with the .sub function
+        // @TODO: replace the following with the .sub function
         balances[msg.sender] -= value;
-        // replace the following with the .add function
+        // @TODO: replace the following with the .add function
         balances[recipient] += value;
     }
 
     function purchase() public payable {
-        // replace the following with the .mul function
+        // @TODO: replace the following with the .mul function
         uint amount = msg.value * exchange_rate;
-        // replace the following with the .add function
+        // @TODO: replace the following with the .add function
         balances[msg.sender] += amount;
         owner.transfer(msg.value);
     }
 
     function mint(address recipient, uint value) public {
         require(msg.sender == owner, "You do not have permission to mint tokens!");
-        // replace the following with the .add function
+        // @TODO: replace the following with the .add function
         balances[recipient] += value;
     }
 }
