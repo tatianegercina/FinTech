33bfac47b0cec9508d62379e90fadbe129d2e52a
Andrew Yang <ayang@trilogyed.com>
2019-12-17 15:52:52 -0500

    moving file

---
+++ 07-Curriculum-Resources/Extra_Content/Solidity/09-Advanced_Smart_Contracts/Unsolved/RideToken.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.0;
+
+contract RideToken {
+
+    // Initializes an address variable
+
+
+    // Creates a mapping variable to map addresses to token balances
+
+
+    // Constructor sets the owner address as the contract creator
+
+
+
+
+    // Sends an amount of newly created coins to an address and can only be called by the contract creator
+
+
+
+
+
+    // Sends an amount of existing coins rom any caller to an address
+
+
+
+
+
+}
