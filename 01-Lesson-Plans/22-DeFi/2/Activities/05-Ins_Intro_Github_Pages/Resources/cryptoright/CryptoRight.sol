342c1ce900fc27456c25514cb29b1c28ed9d1bf5
Alec M. Wantoch <alec@wantoch.net>
2020-01-30 11:36:13 -0500

    updated abi and reference_uri references

--- 01-Lesson-Plans/22-DeFi/2/Activities/05-Ins_Intro_Github_Pages/Resources/cryptoright/CryptoRight.sol
+++ 01-Lesson-Plans/22-DeFi/2/Activities/05-Ins_Intro_Github_Pages/Resources/cryptoright/CryptoRight.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/drafts/Counters.sol";
-import "../Resources/ICryptoRight.sol";
+import "./ICryptoRight.sol";
 
 contract CryptoRight is ICryptoRight {
 
@@ -18,7 +18,7 @@ contract CryptoRight is ICryptoRight {
 
     event Copyright(uint copyright_id, address owner, string reference_uri);
 
-    event OpenSource(uint copyright_id, string uri);
+    event OpenSource(uint copyright_id, string reference_uri);
 
     event Transfer(uint copyright_id, address new_owner);
 
