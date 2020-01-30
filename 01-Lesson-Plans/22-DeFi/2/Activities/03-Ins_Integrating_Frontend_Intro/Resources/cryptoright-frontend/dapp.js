6ff2de9c4d54238ef8960d90273d426f7f0d598b
Alec M. Wantoch <alec@wantoch.net>
2020-01-30 12:13:17 -0500

    Unit 22.2 Move method calls

--- 01-Lesson-Plans/22-DeFi/2/Activities/03-Ins_Integrating_Frontend_Intro/Resources/cryptoright-frontend/dapp.js
+++ 01-Lesson-Plans/22-DeFi/2/Activities/03-Ins_Integrating_Frontend_Intro/Resources/cryptoright-frontend/dapp.js
@@ -104,13 +104,19 @@ const dApp = {
       M.toast({ html: `Success. Reference URI located at ${reference_uri}.` });
       M.toast({ html: "Sending to blockchain..." });
 
-      const method = $("#dapp-opensource-toggle").prop("checked") ? this.contract.methods.openSourceWork(reference_uri) : this.contract.methods.copyrightWork(reference_uri);
-
-      await method.send({from: this.accounts[0]})
-      .on("receipt", (receipt) => {
-        M.toast({ html: "Transaction Mined! Refreshing UI..." });
-        location.reload();
-      });
+      if ($("#dapp-opensource-toggle").prop("checked")) {
+        this.contract.methods.openSourceWork(reference_uri).send({from: this.accounts[0]})
+        .on("receipt", (receipt) => {
+          M.toast({ html: "Transaction Mined! Refreshing UI..." });
+          location.reload();
+        });
+      } else {
+        this.contract.methods.copyrightWork(reference_uri).send({from: this.accounts[0]})
+        .on("receipt", (receipt) => {
+          M.toast({ html: "Transaction Mined! Refreshing UI..." });
+          location.reload();
+        });
+      }
 
     } catch (e) {
       alert("ERROR:", JSON.stringify(e));
