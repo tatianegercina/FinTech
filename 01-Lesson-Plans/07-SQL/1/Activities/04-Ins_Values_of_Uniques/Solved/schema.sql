51d7d7b00a2bd5836372e0ef62216998cb72defa
Andrew Yang <Andrew@Houses-MBP.home>
2020-03-13 09:01:09 -0400

    updating sql files

--- 01-Lesson-Plans/07-SQL/1/Activities/04-Ins_Values_of_Uniques/Solved/schema.sql
+++ 01-Lesson-Plans/07-SQL/1/Activities/04-Ins_Values_of_Uniques/Solved/schema.sql
@@ -1,45 +1,15 @@
--- Delete the table "people"
-DROP TABLE people;
+-- Delete the table "customer"
+DROP TABLE customer;
 
--- Re-create the table "people" within animals_db
-CREATE TABLE people (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  has_pet BOOLEAN DEFAULT false,
-  pet_type VARCHAR(10) NOT NULL,
-  pet_name VARCHAR(30),
-  pet_age INT
+-- Re-create the table "customer" within mortgage_db
+CREATE TABLE customer (
+  customer_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30),
+  gender VARCHAR(30),
+  age INT,
+  address VARCHAR(50),
+  city VARCHAR(50),
+  state CHAR(2),
+  zip_code CHAR(5)
 );
-
--- Insert data into the table
-INSERT INTO people (name, has_pet, pet_type, pet_name, pet_age)
-VALUES ('Jacob', true, 'dog', 'Misty', 10),
-  ('Ahmed', true, 'rock', 'Rockington', 100),
-  ('Ahmed', true, 'rock', 'Rockington', 100),
-  ('Peter', true, 'cat', 'Franklin', 2),
-  ('Dave', true, 'dog', 'Queso', 1),
-  ('Dave', true, 'dog', 'Pringles', 7);
-
--- Query all fields from the table
-SELECT *
-FROM people;
-
--- Query the data to return all the rows containing the name "Dave"
-SELECT id, name, pet_name, pet_age
-FROM people
-WHERE name = 'Dave';
-
--- Update a single row to change the `pet_name` and `pet_age` column data
-UPDATE people
-SET has_pet = true, pet_name = 'Rocket', pet_age = 8
-WHERE id = 6;
-
-SELECT *
-FROM people;
-
--- Delete the duplicate entry using a unique id
-DELETE FROM people
-WHERE id = 3;
-
-SELECT *
-FROM people;
--- 01-Lesson-Plans/07-SQL/1/Activities/04-Ins_Values_of_Uniques/Solved/seed.sql
+++ 01-Lesson-Plans/07-SQL/1/Activities/04-Ins_Values_of_Uniques/Solved/seed.sql
@@ -1,45 +1,10 @@
--- Delete the table "people"
-DROP TABLE people;
-
--- Re-create the table "people" within animals_db
-CREATE TABLE people (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  has_pet BOOLEAN DEFAULT false,
-  pet_type VARCHAR(10) NOT NULL,
-  pet_name VARCHAR(30),
-  pet_age INT
-);
-
 -- Insert data into the table
-INSERT INTO people (name, has_pet, pet_type, pet_name, pet_age)
-VALUES ('Jacob', true, 'dog', 'Misty', 10),
-  ('Ahmed', true, 'rock', 'Rockington', 100),
-  ('Ahmed', true, 'rock', 'Rockington', 100),
-  ('Peter', true, 'cat', 'Franklin', 2),
-  ('Dave', true, 'dog', 'Queso', 1),
-  ('Dave', true, 'dog', 'Pringles', 7);
-
--- Query all fields from the table
-SELECT *
-FROM people;
-
--- Query the data to return all the rows containing the name "Dave"
-SELECT id, name, pet_name, pet_age
-FROM people
-WHERE name = 'Dave';
-
--- Update a single row to change the `pet_name` and `pet_age` column data
-UPDATE people
-SET has_pet = true, pet_name = 'Rocket', pet_age = 8
-WHERE id = 6;
-
-SELECT *
-FROM people;
-
--- Delete the duplicate entry using a unique id
-DELETE FROM people
-WHERE id = 3;
-
-SELECT *
-FROM people;
+INSERT INTO customer
+(first_name, last_name, gender, age, address, city, state, zip_code)
+VALUES
+('Michael', 'Meyer', 'Male', 24, '1021 Eddie Knolls Apt. 087', 'South Geraldton', 'RI', 43709),
+('Cindy', 'Stephens', 'Female', 23, '838 Brown Street', 'East Christina', 'MT', 07829),
+('John', 'Jackson', 'Male', 34, '5319 Candice Motorway', 'Adkinstown', 'AZ', 89721),
+('Alexander', 'Martinez', 'Male', 32, 'USNS Mosley', 'FPO', 'AA', 24673),
+('Alexander', 'Martinez', 'Male', 32, 'USNS Mosley', 'FPO', 'AA', 24673),
+('Michael', 'Meyer', 'Male', 24, '1021 Eddie Knolls Apt. 087', 'South Geraldton', 'RI', 43709);
