a652ca826325de2e941d7b66ebd05fa78b42cae0
Andrew Yang <Andrew@Houses-MBP.home>
2020-03-19 03:01:12 -0400

    updating sql files

--- 01-Lesson-Plans/07-SQL/2/Activities/09-Stu_View_Room_Queries/Solved/query.sql
+++ 01-Lesson-Plans/07-SQL/2/Activities/09-Stu_View_Room_Queries/Solved/query.sql
@@ -1,37 +1,12 @@
--- Create the subquery
-SELECT title,
-(SELECT COUNT(inventory.film_id)
-	FROM inventory
-	WHERE film.film_id = inventory.film_id ) AS "Number of Copies"
-FROM film;
 
--- Create View
-CREATE VIEW title_count AS
-SELECT title,
-(SELECT COUNT(inventory.film_id)
-	FROM inventory
-	WHERE film.film_id = inventory.film_id ) AS "Number of Copies"
-FROM film;
-
--- Query the view to the titles with 7 copies
-SELECT title, "Number of Copies"
-FROM title_count
-WHERE "Number of Copies" = 7;
-
-
-
-
-
-
---NEW
-
-select customer_id, SUM(amount) as total_amount
+CREATE VIEW customer_revenues AS
+select customer_id, COUNT(payment_id) as payment_count, SUM(amount) as total_amount
 from payment
 GROUP BY customer_id
-ORDER BY SUM(amount) DESC
+ORDER BY SUM(amount) DESC;
 
-
-select CAST(payment_date as DATE), SUM(amount) as total_amount
+CREATE VIEW daily_revenues AS
+select CAST(payment_date as DATE), COUNT(payment_id) as payment_count, SUM(amount) as total_amount
 FROM payment
 GROUP BY CAST(payment_date as DATE)
-ORDER BY CAST(payment_date as DATE) DESC
+ORDER BY CAST(payment_date as DATE) DESC;
--- 01-Lesson-Plans/07-SQL/2/Activities/09-Stu_View_Room_Queries/Solved/schema.sql
+++ 01-Lesson-Plans/07-SQL/2/Activities/09-Stu_View_Room_Queries/Solved/schema.sql
@@ -1,37 +1,21 @@
--- Create the subquery
-SELECT title,
-(SELECT COUNT(inventory.film_id)
-	FROM inventory
-	WHERE film.film_id = inventory.film_id ) AS "Number of Copies"
-FROM film;
+CREATE TABLE payment (
+    payment_id integer DEFAULT nextval('payment_payment_id_seq'::regclass) NOT NULL,
+    customer_id smallint NOT NULL,
+    staff_id smallint NOT NULL,
+    rental_id integer NOT NULL,
+    amount numeric(5,2) NOT NULL,
+    payment_date timestamp without time zone NOT NULL
+);
 
--- Create View
-CREATE VIEW title_count AS
-SELECT title,
-(SELECT COUNT(inventory.film_id)
-	FROM inventory
-	WHERE film.film_id = inventory.film_id ) AS "Number of Copies"
-FROM film;
-
--- Query the view to the titles with 7 copies
-SELECT title, "Number of Copies"
-FROM title_count
-WHERE "Number of Copies" = 7;
-
-
-
-
-
-
---NEW
-
-select customer_id, SUM(amount) as total_amount
-from payment
-GROUP BY customer_id
-ORDER BY SUM(amount) DESC
-
-
-select CAST(payment_date as DATE), SUM(amount) as total_amount
-FROM payment
-GROUP BY CAST(payment_date as DATE)
-ORDER BY CAST(payment_date as DATE) DESC
+CREATE TABLE customer (
+    customer_id integer DEFAULT nextval('customer_customer_id_seq'::regclass) NOT NULL,
+    store_id smallint NOT NULL,
+    first_name character varying(45) NOT NULL,
+    last_name character varying(45) NOT NULL,
+    email character varying(50),
+    address_id smallint NOT NULL,
+    activebool boolean DEFAULT true NOT NULL,
+    create_date date DEFAULT ('now'::text)::date NOT NULL,
+    last_update timestamp without time zone DEFAULT now(),
+    active integer
+);
