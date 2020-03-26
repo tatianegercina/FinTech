28bbfd1505cc4d4fc50d109cdb35122b016daed5
jhillgithub <jhillgithub@users.noreply.github.com>
2020-03-26 10:23:43 -0500

    removes gif from archiver for performance

--- .archiver.sh
+++ .archiver.sh
@@ -19,7 +19,7 @@ find 01-Lesson-Plans -type d -mindepth 1 -maxdepth 1 -exec cp -r {} build/01-Les
 pushd build/01-Lesson-Plans
 
 echo "Compress Lesson Images..."
-for f in $(find -E . -iregex ".*\.(jpg|gif|png|jpeg)" -mindepth 1); do mv $f $f.raw && imagemin $f.raw > $f && rm $f.raw; done
+for f in $(find -E . -iregex ".*\.(jpg|png|jpeg)" -mindepth 1); do mv $f $f.raw && imagemin $f.raw > $f && rm $f.raw; done
 echo "Zip Lesson Folders..."
 find . -type d -maxdepth 1 -mindepth 1 -exec zip -rm {}.zip {} \;
 
@@ -33,7 +33,7 @@ find 02-Homework -type d -mindepth 1 -maxdepth 1 -exec cp -r {} build/02-Homewor
 pushd build/02-Homework
 
 echo "Compress Homework Images..."
-for f in $(find -E . -iregex ".*\.(jpg|gif|png|jpeg)" -mindepth 1); do mv $f $f.raw && imagemin $f.raw > $f && rm $f.raw; done
+for f in $(find -E . -iregex ".*\.(jpg|png|jpeg)" -mindepth 1); do mv $f $f.raw && imagemin $f.raw > $f && rm $f.raw; done
 echo "Zip Homework Folders..."
 find . -type d -maxdepth 1 -mindepth 1 -exec zip -rm {}.zip {} \;
 
@@ -47,7 +47,7 @@ find 03-Projects -type d -mindepth 1 -maxdepth 1 -exec cp -r {} build/03-Project
 pushd build/03-Projects
 
 echo "Compress Project Images..."
-for f in $(find -E . -iregex ".*\.(jpg|gif|png|jpeg)" -mindepth 1); do mv $f $f.raw && imagemin $f.raw > $f && rm $f.raw; done
+for f in $(find -E . -iregex ".*\.(jpg|png|jpeg)" -mindepth 1); do mv $f $f.raw && imagemin $f.raw > $f && rm $f.raw; done
 echo "Zip Project folders..."
 find . -type d -maxdepth 1 -mindepth 1 -exec zip -rm {}.zip {} \;
 
