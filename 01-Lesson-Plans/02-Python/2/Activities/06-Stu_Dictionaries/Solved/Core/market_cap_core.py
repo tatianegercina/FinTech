cc536fefd2af68e0ff7011696d921b006b0f2243
Jose Arturo Mora Soto <jarturomora@trilogyed.com>
2020-05-11 18:25:49 -0500

    Adds changes requested after QA to Day 2

--- 01-Lesson-Plans/02-Python/2/Activities/06-Stu_Dictionaries/Solved/Core/market_cap_core.py
+++ 01-Lesson-Plans/02-Python/2/Activities/06-Stu_Dictionaries/Solved/Core/market_cap_core.py
@@ -8,49 +8,49 @@ bank names associated with the corresponding market cap ranges.
 
 # Banks and Market Caps
 #-----------------------
-# JP Morgan Chase: 327
-# Bank of America: 302
-# Citigroup: 170
+# National Bank of Canada: 327
+# Toronto-Dominion Bank: 302
+# Royal Bank of Canada: 173
 # Wells Fargo: 273
 # Goldman Sachs: 87
 # Morgan Stanley: 72
-# U.S. Bancorp: 83
+# Canadian Imperial Bank of Commerce: 83
 # TD Bank: 108
-# PNC Financial Services: 67
+# Bank of Montreal: 67
 # Capital One: 47
 # FNB Corporation: 4
-# First Hawaiian Bank: 3
+# Laurentian Bank of Canada: 3
 # Ally Financial: 12
-# Wachovia: 145
-# Republic Bancorp: .97
+# Montreal Trust Company: 145
+# Canadian Western Bank: .97
 
 # Initialize a dictionary of banks and market caps (in billions)
 banks = {
-    "JP Morgan Chase": 327,
-    "Bank of America": 302,
-    "Citigroup": 173,
+    "National Bank of Canada": 327,
+    "Toronto-Dominion Bank": 302,
+    "Royal Bank of Canada": 173,
     "Wells Fargo": 273,
     "Goldman Sachs": 87,
     "Morgan Stanley": 72,
-    "U.S. Bancorp": 83,
+    "Canadian Imperial Bank of Commerce": 83,
     "TD Bank": 108,
-    "PNC Financial Services": 67,
+    "Bank of Montreal": 67,
     "Capital One": 47,
     "FNB Corporation": 4,
-    "First Hawaiian Bank": 3,
+    "Laurentian Bank of Canada": 3,
     "Ally Financial": 12,
-    "Wachovia": 145,
-    "Republic Bancorp": .97
+    "Montreal Trust Company": 145,
+    "Canadian Western Bank": .97
 }
 
 # Change the market cap for 'Citigroup'
-banks['Citigroup'] = 170
+banks['Royal Bank of Canada'] = 170
 
 # Add a new bank and market cap pair
-banks['American Express'] = 33
+banks['Scotiabank'] = 33
 
 # Remove a bank from the dictionary
-del banks['Wachovia']
+del banks['Montreal Trust Company']
 
 # Print the modified dictionary
 print(banks)
