1f181cdc8d11491623ffdea7f7389203e366d21d
Lori <lorieharris@gmail.com>
2020-09-14 13:40:04 -0400

    Corrected Readme and Unsolved to match solved.

--- 01-Lesson-Plans/03-Python-Pandas/1/Activities/05-inputs-01/Unsolved/inputs-01.py
+++ 01-Lesson-Plans/03-Python-Pandas/1/Activities/05-inputs-01/Unsolved/inputs-01.py
@@ -4,11 +4,10 @@
 # Then print the string "Hello" concatenated with the variable `welcome_name`.
 
 
-# Declare a variable `question_sandwich` as an input with a string of "Are you here for a sandwich? (Y/N) ".
+# Declare a variable `question_sandwich` as an input with a string of "Are you here for a sandwich? (Yes or No?) ".
 
 
-# If `question_sandwich` is equal to "Y" or "y" declare a variable `food_prompt` as an input with a string of "What kind of sandwich would you like?".
+# If `question_sandwich` is equal to true declare a variable `food_prompt` as an input with a string of "What kind of sandwich would you like?".
 # Then print a string of "Please wait 10 min for your " concatenated with the variable `food_prompt`.
-# Else If `question_sandwich` is "N" or "n", print a string of "If you don't want a sandwich what are you here for?!".
-# Else print a string of "You did not write Y or N!"
-
+# Else If `question_sandwich` is false, print a string of "If you don't want a sandwich what are you here for?!".
+# Else print a string of "You did not write Yes or No!"
